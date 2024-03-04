aa68c4df220757a058cf9b1c44715ff8f6ce11b3
WebDev1980 <113666624+WebDev1980@users.noreply.github.com>
2024-03-04 14:17:25 -0600

    Delete oneDelayonly.js

--- oneDelayonly.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var bg2 = document.querySelector(".background2");
-    bg2.addEventListener("animationend", function() {
-        this.style.animation = "none";
-    });
-});
